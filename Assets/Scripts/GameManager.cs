de1d7e619bddb876984f9d22a5b2995326ab9f04
Darren Scott <darrenpipi8@gmail.com>
2025-01-13 13:15:15 +0100

    Bool electricity on

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public bool elctricityOn = true;
+
     // Start is called before the first frame update
     void Start()
     {
