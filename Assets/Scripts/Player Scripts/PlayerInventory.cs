014d2dd8e1194f0ca7b7ffbe7fdb922b27f039b8
mystiified <152400721+mystiified@users.noreply.github.com>
2025-01-13 03:41:47 +0100

    Inventory development #1

--- Assets/Scripts/Player Scripts/PlayerInventory.cs
+++ Assets/Scripts/Player Scripts/PlayerInventory.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
+using static UnityEditor.Progress;
 
 public class PlayerInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Item> items = new List<Item>();
+    
+    private int maxSlots = 4;
+
+    public delegate void OnInventoryUpdated();
+    public event OnInventoryUpdated InventoryUpdated;
+
+    public void AddItem(Item item)
     {
-        
+        if (items.Count >= maxSlots)
+        {
+            Debug.Log("Inventory is full!");
+            return;
+        }
+
+        items.Add(item);
+        Debug.Log($"Added item: {item.itemName}");
+        InventoryUpdated?.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveItem(Item item)
     {
-        
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+            Debug.Log($"Removed item: {item.itemName}");
+            InventoryUpdated?.Invoke();
+        }
+        else
+        {
+            Debug.Log("Item not found in inventory!");
+        }
+    }
+
+    public void UseItem(Item item)
+    {
+        if (items.Contains(item))
+        {
+            item.Use();
+            if (!item.isStackable)
+            {
+                RemoveItem(item);
+            }
+        }
+        else
+        {
+            Debug.Log("Item not in inventory!");
+        }
     }
 }
