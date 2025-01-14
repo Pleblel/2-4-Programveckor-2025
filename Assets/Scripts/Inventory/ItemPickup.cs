1fae4a9616430ad99425b81388c013f4a4635d56
mystiified <152400721+mystiified@users.noreply.github.com>
2025-01-14 11:10:43 +0100

    More Inventory Development

---
+++ Assets/Scripts/Inventory/ItemPickup.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    [SerializeField] Item item;
+    [SerializeField] InventoryManager IManager;
+
+    void Pickup()
+    {
+        InventoryManager.Instance.Add(item);
+        Destroy(gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        Pickup();
+    }
+}
