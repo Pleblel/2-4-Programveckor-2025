00e62c4b325bafe2d2b969af7a88b4b9de148e98
Darren Scott <darrenpipi8@gmail.com>
2025-01-16 11:16:06 +0100

    Electrcity fix

--- Assets/Scripts/Electricity.cs
+++ Assets/Scripts/Electricity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Electricity : MonoBehaviour
 {
     float damage = 10f;
+    public float knockBackForce = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +26,30 @@ public class Electricity : MonoBehaviour
             BaseEntity player = collision.collider.GetComponent<BaseEntity>();
 
             if (player != null)
-                player.TakeDamage(damage);
+            {
+                // player.TakeDamage(damage);
+                Debug.Log("Electrified");
+                Rigidbody playerRb = collision.collider.GetComponent<Rigidbody>();
+                PlayerMovement playerController = collision.collider.GetComponent<PlayerMovement>();
+
+                if (playerRb != null && playerController != null)
+                {
+
+                    playerController.isBeingKnockedBack = true;
+                    Vector3 knockbackDirection = new Vector3(collision.collider.transform.position.x - transform.position.x, 0, 0).normalized;
+
+
+                    if(playerController.isBeingKnockedBack)
+                    {
+                        playerRb.AddForce(knockbackDirection * knockBackForce, ForceMode.Impulse);
+                    }
+
+                    playerController.isBeingKnockedBack = false;
+
+                }
+            }
+               
         }
     }
+
 }
